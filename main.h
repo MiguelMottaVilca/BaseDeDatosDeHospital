0e8e3000b14691dc0c3c4c94ced95b92ce86cd9e
Miguel Motta <miguelmotta@MacBook-Pro-de-Miguel.local>
2019-12-03 20:22:42 -0500

    first commit

---
+++ main.h
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "date.h"
+#include "usuario.h"
+#include "persona.h"
+#include "medico.h"
+#include "enfermeros.h"
+#include "paciente.h"
+#include "medicamentos.h"
+
+void agregarPaciente(void){
+
+    std::cout <<"     REGISTRANDO UN PACIENTE"<<'\n'<<'\n';
+    ostringstream aux;
+    string text;
+
+    Paciente tem ;
+
+    int confirmar;
+    std::cout << '\n'<<'\n';
+    system("clear");
+    std::cout << "      VERIFICAR SU INFORMACION        "<<'\n'<<'\n';
+    tem.mostrarPaciente();
+
+    std::cout<<'\n'<<'\n';
+    std::cout << "0  MODIFICAR INFORMACION        "<<'\n';
+    std::cout << "1  GUARDAR SU INFORMACION        "<<'\n'<<'\n';
+    std::cout<<"DIGITE NUMERO : " ; std::cin >>confirmar;
+
+    if ( confirmar == 0){
+        while(true){
+            int num;
+            std::cout << "      MODIFICAR INFORMACION        "<<'\n'<<'\n';
+            std::cout << "1  MODIFICAR APELLIDO        "<<'\n';
+            std::cout << "2  MODIFICAR NOMBRE        "<<'\n';
+            std::cout << "3  MODIFICAR DNI        "<<'\n';
+            std::cout << "4  MODIFICAR CUENTA        "<<'\n';
+            std::cout << "5  MODIFICAR CONTRASEÑA        "<<'\n'<<'\n';
+            std::cout << "DIGITE NUMERO : ";std::cin >> num;
+            switch(num){
+                case 1:{
+                    std::cout<<"ESTAS EN EL CASO 1"<<endl;
+                }
+                default: {
+                    system("clear");
+                    std::cout << "DIGITE EN EL RANGO DE NUMEROS" <<'\n';
+                }
+            }
+
+        }
+    }
+    if ( confirmar == 1 ){
+        aux << tem.getApellido() << "___" << tem.getNombre() << "___" << tem.getDni() << "___" << tem.getRegistro()  
+        << "___" << tem.getUsuario() << "___" << tem.getContra() << "*****" ;
+        text = aux.str();
+
+        ofstream archivo;
+
+        archivo.open("pacientes.txt",ios::app);
+        if (archivo.fail())
+                std::cout<<"No se puedo abrir el archivo"<<'\n';
+
+        archivo<< text;
+        archivo.close();
+    }
+
+   
+
+}
+
+void agregarMedico(){
+
+    ostringstream aux;
+    string text;
+    Usuario x;
+
+    Medico tem ;
+
+    aux << tem.getApellido() << "___" << tem.getNombre() << "___" << tem.getDni() << "___" << tem.getEspecialidad() << "*****" ;
+
+    text = aux.str();
+
+    ofstream archivo;
+
+    archivo.open("medicos.txt",ios::app);
+    if (archivo.fail())
+        std::cout<<"No se puedo abrir el archivo"<<'\n';
+
+    archivo<< text;
+
+    archivo.close();
+
+}
+
+void agregarEnfermeros(){
+
+    ostringstream aux;
+    string text;
+    Usuario x;
+
+    Enfermero tem;
+
+    aux << tem.getApellido() << "___" << tem.getNombre() << "___" << tem.getDni() << "___" << tem.getEspecialidad() << "*****" ;
+
+    text = aux.str();
+
+    ofstream archivo;
+
+    archivo.open("enfermeros.txt",ios::app);
+    if (archivo.fail())
+        std::cout<<"No se puedo abrir el archivo"<<'\n';
+
+    archivo<< text;
+
+    archivo.close();
+
+}
+
+void agregarMedicamentos(){
+
+    ostringstream aux;
+    string text;
+    Medicamento x;
+
+    aux << x.getNombre() << "__" << x.getCantidad() <<"*****";
+
+    text = aux.str();
+
+    ofstream archivo;
+
+    archivo.open("usuarios.txt",ios::app);
+    if (archivo.fail())
+        std::cout<<"No se puedo abrir el archivo"<<'\n';
+
+    archivo<< text;
+
+    archivo.close();
+}
